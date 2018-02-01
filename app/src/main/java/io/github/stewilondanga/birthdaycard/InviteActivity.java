da8e9d7cd41685bd38db2c5fea7bbedb7ccf418a
Stewart ilondanga <stilondanga@gmail.com>
2018-02-01 16:46:01 +0300

    add app/src/main/java/io/github/stewilondanga/birthdaycard/InviteActivity.java

---
+++ app/src/main/java/io/github/stewilondanga/birthdaycard/InviteActivity.java
@@ -0,0 +1,13 @@
+package io.github.stewilondanga.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InviteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite);
+    }
+}
